7987c10bf91aca4a637e0c2b1a2c8e7c1a037672
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-08-19 14:26:42 -0300

    feat: new routes at Router and finished Header

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import Meals from '../pages/Meals';
 import Layout from '../components/Layout';
+import Drinks from '../pages/Drinks';
+import Profile from '../pages/Profile';
 
 function Router() {
   return (
@@ -9,7 +11,15 @@ function Router() {
       <Route path="/" element={ <Login /> } />
       <Route path="" element={ <Layout /> }>
         <Route path="/meals" element={ <Meals /> } />
+        <Route path="/drinks" element={ <Drinks /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/done-recipes" element={ <h1>Not Found</h1> } />
+        <Route path="/favorite-recipes" element={ <h1>Not Found</h1> } />
       </Route>
+      <Route path="/meals/:id-da-receita" element={ <h1>Not Found</h1> } />
+      <Route path="/drinks/:id-da-receita" element={ <h1>Not Found</h1> } />
+      <Route path="/meals/:id-da-receita/in-progress" element={ <h1>Not Found</h1> } />
+      <Route path="/drinks/:id-da-receita/in-progress" element={ <h1>Not Found</h1> } />
     </Routes>
   );
 }
