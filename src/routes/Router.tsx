547364ab243c0ee51b7a234d414d384e4372b4f8
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-08-19 13:42:56 -0300

    feat: Layout and Header

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import Meals from '../pages/Meals';
+import Layout from '../components/Layout';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      <Route path="/meals" element={ <Meals /> } />
+      <Route path="" element={ <Layout /> }>
+        <Route path="/meals" element={ <Meals /> } />
+      </Route>
     </Routes>
   );
 }
