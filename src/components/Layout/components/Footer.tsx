007260b9d845804d03d0c5a37a9641a4bbedf102
Matheus Bandeira <maabann@gmail.com>
2023-08-21 16:37:22 -0300

    estilizado o componente footer e fixado na tela

--- src/components/Layout/components/Footer.tsx
+++ src/components/Layout/components/Footer.tsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
     <footer data-testid="footer" id="footer">
-      <button data-testid="drinks-bottom-btn">
+      <button data-testid="drinks-bottom-btn" id="footer-btn-Left">
         <img src="src/images/drinkIcon.svg" alt="drink-button" />
       </button>
-      <button data-testid="meals-bottom-btn">
+      <button data-testid="meals-bottom-btn" id="footer-btn-Right">
         <img src="src/images/mealIcon.svg" alt="food-button" />
       </button>
     </footer>
