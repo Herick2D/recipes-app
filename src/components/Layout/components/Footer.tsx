b467718c8ed0534871780e7d7772b76cd0d24f06
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-08-22 15:29:47 -0300

    style: changed css to styled components

--- src/components/Layout/components/Footer.tsx
+++ src/components/Layout/components/Footer.tsx
@@ -1,27 +1,28 @@
 import { useNavigate } from 'react-router-dom';
+import { ButtonStyled, FooterStyled } from './styles/FooterStyles';
 
 function Footer() {
   const navigate = useNavigate();
 
   return (
-    <footer data-testid="footer" id="footer">
-      <button id="footer-btn-Left" onClick={ () => navigate('/drinks') }>
+    <FooterStyled data-testid="footer" id="footer">
+      <ButtonStyled onClick={ () => navigate('/drinks') }>
         <img
           className="footer-img"
           src="src/images/drinkIcon.svg"
           alt="drink-button"
           data-testid="drinks-bottom-btn"
         />
-      </button>
-      <button id="footer-btn-Right" onClick={ () => navigate('/meals') }>
+      </ButtonStyled>
+      <ButtonStyled onClick={ () => navigate('/meals') }>
         <img
           className="footer-img"
           src="src/images/mealIcon.svg"
           alt="food-button"
           data-testid="meals-bottom-btn"
         />
-      </button>
-    </footer>
+      </ButtonStyled>
+    </FooterStyled>
   );
 }
 
