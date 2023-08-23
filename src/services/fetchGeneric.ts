119f8170a491f31c8bd8e37afeac06dbcf576a37
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-08-23 10:47:51 -0300

    fix: fetchGeneric

--- src/services/fetchGeneric.ts
+++ src/services/fetchGeneric.ts
@@ -1,5 +1,12 @@
 export const fetchGeneric = async (pathname: string) => {
-  const response = pathname === '/meals' ? await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=') : await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
-  const data = await response.json();
-  return data;
+  if (pathname === '/meals') {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    return data;
+  }
+  if (pathname === '/drinks') {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    return data;
+  }
 };
