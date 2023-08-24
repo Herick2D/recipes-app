08d9ad3b86d8abc16dbfcf7dd51428d7c84bd9b4
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-08-24 18:48:11 -0300

    fix directory

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CategoriesButtons from '../Recipes/components/CategoriesButtons';
+import CategoriesButtons from './components/CategoriesButtons';
 
 function Layout() {
   const { pathname } = useLocation();
