80f9808c993f1154330f3847d3d951c02f677636
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-08-25 15:31:09 -0300

    feat: Carousel

--- src/pages/Recipe/index.tsx
+++ src/pages/Recipe/index.tsx
@@ -2,12 +2,15 @@ import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useFetchById from '../../hooks/useFetchById';
 import RecipeInfos from './components/RecipeInfos';
+import useFetchGeneric from '../../hooks/useFetchGeneric';
+import Carousel from './components/Carousel';
 
 function Recipe() {
   const [entriesRecipe, setEntriesRecipe] = useState<[string, string][]>([]);
   const { data, loading } = useFetchById();
   const { pathname } = useLocation();
   const location = pathname.split('/')[1];
+  const { drinksRecipes, mealsRecipes } = useFetchGeneric();
 
   useEffect(() => {
     if (data[0]) {
@@ -20,7 +23,16 @@ function Recipe() {
   }
 
   return (
-    <RecipeInfos location={ location } data={ data } entriesRecipe={ entriesRecipe } />
+    <>
+      <RecipeInfos location={ location } data={ data } entriesRecipe={ entriesRecipe } />
+      <div>
+        {drinksRecipes.length > 1 ? (
+          <Carousel drinks={ drinksRecipes.slice(0, 6) } />
+        ) : (
+          <Carousel meals={ mealsRecipes.slice(0, 6) } />
+        )}
+      </div>
+    </>
   );
 }
 
