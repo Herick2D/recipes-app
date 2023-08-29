7a2bb8d7ac14199eaed6e1cf3d4e2de7691605a3
Danilo <danilocbertolini@gmail.com>
2023-08-29 20:29:15 -0300

    feat:implementation of profile screen buttons

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,9 +4,20 @@ function Profile() {
   const { value, updateValue } = useLocalStorage('user', JSON.stringify({ email: '' }));
   const { email } = JSON.parse(value);
   return (
-    <h1 data-testid="profile-email">
-      {email}
-    </h1>
+    <>
+      <h1 data-testid="profile-email">
+        {email}
+      </h1>
+      <button data-testid="profile-done-btn">
+        Done Recipes
+      </button>
+      <button data-testid="profile-favorite-btn">
+        Favorite Recipes
+      </button>
+      <button data-testid="profile-logout-btn">
+        Logout
+      </button>
+    </>
   );
 }
 
