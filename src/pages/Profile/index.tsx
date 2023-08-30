cc7a3c80cedbfc553e8bf5d8b59041a7f1adb52e
Danilo <danilocbertolini@gmail.com>
2023-08-30 20:19:12 -0300

    feat: testes pagina profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,20 +1,34 @@
+import { useNavigate } from 'react-router-dom';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
 function Profile() {
-  const { value, updateValue } = useLocalStorage('user', JSON.stringify({ email: '' }));
+  const { value } = useLocalStorage('user', JSON.stringify({ email: '' }));
   const { email } = JSON.parse(value);
+  const navigate = useNavigate();
   return (
     <>
       <h1 data-testid="profile-email">
         {email}
       </h1>
-      <button data-testid="profile-done-btn">
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => navigate('/done-recipes') }
+      >
         Done Recipes
       </button>
-      <button data-testid="profile-favorite-btn">
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => navigate('/favorite-recipes') }
+      >
         Favorite Recipes
       </button>
-      <button data-testid="profile-logout-btn">
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          navigate('/');
+          localStorage.clear();
+        } }
+      >
         Logout
       </button>
     </>
